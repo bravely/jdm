8a0ddf0dee0f2ebd03deec22a25b064fd2da4691
Jake Demarest-Mays <jake.m.mays@gmail.com>
2020-05-09 21:10:16 -0600

    tmp

---
+++ src/Header.svelte
@@ -0,0 +1,27 @@
+<script>
+</script>
+
+<header>
+  <h1>Jake Demarest-Mays</h1>
+  <nav>
+    <form>
+      <label class="search-label" for="search" aria-labelledby="searchTitle">
+        <title id="searchTitle">Search</title>
+        🔍
+      </label>
+    </form>
+  </nav>
+</header>
+
+<style>
+header {
+  display: flex;
+  align-items: center;
+  padding: 0 3em;
+  background: rgb(0,0,0);
+  background: linear-gradient(0deg, rgba(0,0,0,0.5) 0%, rgba(255,255,255,0) 40%);
+}
+nav {
+  margin-left: auto;
+}
+</style>
